8bad3e5f92166e32e4248093e3217b4072d5ab9f
rishiVaraha <rishi.kumar@varahaworld.com>
2025-01-03 10:55:14 +0530

    change name

--- app/(Components)/ProductHeader/product-header.tsx
+++ app/(Components)/ProductHeader/product-header.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ProductsTable } from "../ProductsTable/product-table";
+import { ProductsTable } from "../ProductsFilter/product-filter";
 
 export function DataTable() {
   return (
